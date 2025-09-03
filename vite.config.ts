02ce1e528baa58aeeb12a1967a7c4f70164e83c7
rahul-error <rahul@example.com>
2025-09-03 21:48:35 +0530

    fix: update vite config for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [
     react(),
   ].filter(Boolean),
@@ -16,4 +12,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "./",
 }));
